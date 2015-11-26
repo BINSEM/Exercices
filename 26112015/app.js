f91ba63853309629e4e63eca6adc952663315ca3
Semere <BINI>
2015-11-26 16:56:49 +0100

    exercices js 26112015

---
+++ 26112015/app.js
@@ -0,0 +1,23 @@
+alert('coucou monde!');
+var foo = "exercices"
+var bar = foo.toLowerCase();
+if (foo == bar) {alert ("much")}
+	else {alert ("not much")}
+var teki = "COUCOR"
+var semi = teki.toLowerCase();
+if (teki == semi)
+	{ alert ("much")}
+else {alert ("not much")}
+
+function comparchaine (str) {
+	var maj = str.toUpperCase();
+	var tag = "<br>";
+	if (str == maj) {
+	document.writeln(str+" it is much");
+	}
+} 
+comparchaine ("blabla  blabla");
+
+var foo = "flafla flafla";
+comparchaine(foo)
+comparchaine("KSDHGSKRDSH");
